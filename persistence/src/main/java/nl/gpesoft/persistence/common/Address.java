8f76dd15f8a7dfa1deda7b38a9df86d190d389c4
peterse73 <peterse73@gmail.com>
2017-05-25 23:39:33 +0200

    Add Address

---
+++ persistence/src/main/java/nl/gpesoft/persistence/common/Address.java
@@ -0,0 +1,43 @@
+package nl.gpesoft.persistence.common;
+
+import java.util.Date;
+
+public class Address {
+
+    private String street;
+    private int number;
+    private String postalCode;
+    private String town;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
