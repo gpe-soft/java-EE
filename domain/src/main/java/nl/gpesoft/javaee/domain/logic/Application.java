9c9f2c08cd66f8aae734a1dd14ce28d3ba3b6926
peterse73 <peterse73@gmail.com>
2017-07-11 22:37:25 +0200

    Add CDI logic

--- domain/src/main/java/nl/gpesoft/javaee/domain/logic/Application.java
+++ domain/src/main/java/nl/gpesoft/javaee/domain/logic/Application.java
@@ -2,12 +2,20 @@ package nl.gpesoft.javaee.domain.logic;
 
 import nl.gpesoft.javaee.domain.entity.Developer;
 import nl.gpesoft.javaee.domain.port.persistence.Repository;
-import nl.gpesoft.javaee.persistence.adapter.RepositoryAdapter;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+@Stateless
 public class Application {
-    public static void main(String[] args) {
-        Developer developer = new Developer();
-        Repository persistencePort = new RepositoryAdapter();
 
+    @Inject
+    public Repository repositoryAdapter;
+
+    public void run() {
+        Developer developer = new Developer();
+        developer.setInitials("A.G.");
+        developer.setSurname("Peterse");
+        this.repositoryAdapter.addDeveloper(developer);
     }
 }
--- persistence/src/main/java/nl/gpesoft/javaee/persistence/adapter/RepositoryAdapter.java
+++ persistence/src/main/java/nl/gpesoft/javaee/persistence/adapter/RepositoryAdapter.java
@@ -3,8 +3,16 @@ package nl.gpesoft.javaee.persistence.adapter;
 import nl.gpesoft.javaee.domain.entity.Developer;
 import nl.gpesoft.javaee.domain.port.persistence.Repository;
 
+import javax.ejb.Singleton;
+
+@Singleton
 public class RepositoryAdapter implements Repository {
+
+    public RepositoryAdapter() {
+        System.out.println("construction of repositoryAdapter");
+    }
+
     public void addDeveloper(Developer developer) {
-        System.out.println("Adding developer");
+        System.out.println("Adding developer: " + developer.getInitials() + " " + developer.getSurname());
     }
 }
