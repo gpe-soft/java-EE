e5574d8ca7372a06b60fe55cb700e8b852b2d65f
agpeterse <peterse73@gmail.com>
2017-07-24 14:39:18 +0200

    Extends functionality of persistenceMapper

--- persistence/src/main/java/nl/gpesoft/javaee/persistence/dao/DeveloperDao.java
+++ persistence/src/main/java/nl/gpesoft/javaee/persistence/dao/DeveloperDao.java
@@ -4,11 +4,18 @@ import nl.gpesoft.javaee.domain.entity.Developer;
 import nl.gpesoft.javaee.persistence.mapper.PersistenceMapper;
 
 import javax.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.List;
 
 @Stateless //EJB to let the container manage transactions
 public class DeveloperDao extends Dao {
 
-     public void addDeveloper (Developer developer) {
-        entityManager.persist(PersistenceMapper.toPersistenceModel(developer));
+    public void addDeveloper(Developer developer) {
+        entityManager.persist(PersistenceMapper.toPersistenceModelDeveloper(developer));
+    }
+
+    public List<Developer> getAllDevelopers() {
+        List<Developer> listAllDevelopers = new ArrayList<Developer>();
+        return listAllDevelopers;
     }
 }
--- persistence/src/main/java/nl/gpesoft/javaee/persistence/mapper/PersistenceMapper.java
+++ persistence/src/main/java/nl/gpesoft/javaee/persistence/mapper/PersistenceMapper.java
@@ -1,14 +1,54 @@
 package nl.gpesoft.javaee.persistence.mapper;
 
+import nl.gpesoft.javaee.domain.entity.Address;
 import nl.gpesoft.javaee.domain.entity.Developer;
+import nl.gpesoft.javaee.domain.entity.Title;
+import nl.gpesoft.javaee.persistence.common.AddressTable;
 import nl.gpesoft.javaee.persistence.model.DeveloperTable;
 
 public class PersistenceMapper {
 
-    public static DeveloperTable toPersistenceModel(Developer developer) {
+    public static DeveloperTable toPersistenceModelDeveloper(Developer developer) {
         DeveloperTable developerTable = new DeveloperTable();
-        developerTable.setInitials(developer.getInitials());
         developerTable.setTitle(developer.getTitle().title());
+        developerTable.setInitials(developer.getInitials());
+        developerTable.setSurname(developer.getSurname());
+        developerTable.setDateOfBirth(developer.getDateOfBirth());
+        developerTable.setSocialSecurityNumber(developer.getSocialSecurityNumber());
+        developerTable.setResidentialAddress(toPersistenceModelAddress(developer.getResidentialAddress()));
+        developerTable.setEmploymentDate(developer.getEmploymentDate());
+        developerTable.setFavouriteProgrammingLanguage(developer.getFavouriteProgrammingLanguage());
         return developerTable;
+    }
+
+    public static Developer toDomainModelDeveloper(DeveloperTable developerTable) {
+        Developer developer = new Developer();
+        developer.setTitle(Title.valueOf(developerTable.getTitle()));
+        developer.setInitials(developerTable.getInitials());
+        developer.setSurname(developerTable.getSurname());
+        developer.setDateOfBirth(developerTable.getDateOfBirth());
+        developer.setSocialSecurityNumber(developerTable.getSocialSecurityNumber());
+        developer.setResidentialAddress(toDomainModelAddress(developerTable.getResidentialAddress()));
+        developer.setEmploymentDate(developerTable.getEmploymentDate());
+        developer.setFavouriteProgrammingLanguage(developerTable.getFavouriteProgrammingLanguage());
+        return developer;
+    }
+
+    private static AddressTable toPersistenceModelAddress(Address address) {
+        AddressTable addressTable = new AddressTable();
+        addressTable.setStreet(address.getStreet());
+        addressTable.setNumber(address.getNumber());
+        addressTable.setPostalCode(address.getPostalCode());
+        addressTable.setTown(address.getTown());
+        return addressTable;
+    }
+
+    private static Address toDomainModelAddress(AddressTable addressTable) {
+        Address address = new Address();
+        address.setStreet(addressTable.getStreet());
+        address.setNumber(addressTable.getNumber());
+        address.setPostalCode(addressTable.getPostalCode());
+        address.setTown(addressTable.getTown());
+        return address;
     }
 }
