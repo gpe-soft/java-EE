959cd629ba5ae9746874b94a69964f87119f8491
agpeterse <peterse73@gmail.com>
2017-07-24 16:54:47 +0200

    Add date format

--- domain/src/main/java/nl/gpesoft/javaee/domain/logic/Application.java
+++ domain/src/main/java/nl/gpesoft/javaee/domain/logic/Application.java
@@ -6,6 +6,8 @@ import nl.gpesoft.javaee.domain.entity.Title;
 import nl.gpesoft.javaee.domain.port.persistence.PersistenceRepository;
 
 import javax.inject.Inject;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -19,7 +21,14 @@ public class Application {
         developer.setTitle(Title.BSC);
         developer.setInitials("T.F.");
         developer.setSurname("Fortran");
-        developer.setDateOfBirth(new Date(19710901));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            developer.setDateOfBirth(dateFormat.parse("01/09/1971"));
+            developer.setEmploymentDate(dateFormat.parse("07/06/1992"));
+
+        } catch (ParseException parseException) {
+            parseException.printStackTrace();
+        }
         developer.setSocialSecurityNumber(999912345678L);
         Address residentialAddress = new Address();
         residentialAddress.setStreet("5th Avenue west");
@@ -27,7 +36,6 @@ public class Application {
         residentialAddress.setPostalCode("YT-67890");
         residentialAddress.setTown("New York");
         developer.setResidentialAddress(residentialAddress);
-        developer.setEmploymentDate(new Date(19920607));
         developer.setFavouriteProgrammingLanguage("Pascal");
         persistenceRepositoryAdapter.addDeveloper(developer);
     }
